1fa6caad7ab5300a59b108a3885feed012b33ba2
justo-marcella <justoamarcella@gmail.com>
2021-03-16 11:41:45 -0300

    app react inicializado

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Banner from './components/Banner/Banner';
+import Header from './components/Header/Header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <main>
+      <Banner/>
+      </main>
+      
+    </div>
+  );
+}
+
+export default App;
