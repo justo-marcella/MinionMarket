d3395b5089b272a3ce87783102ed9f4c443949bb
justo-marcella <justoamarcella@gmail.com>
2021-03-20 10:16:35 -0300

    Ajuste de responsividade

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <footer className={styles.footerContainer}>
             <nav className={styles.menuFooter}>
-                <li>Feito com amor por Marcella Justo</li>
+                <p>Feito com amor por Marcella Justo</p>
                 <ul className={styles.icons}>
                     <li><a href='https://www.linkedin.com/in/marcella-justo-1706/'><img src='img/linkedin.png' alt='link para linkedin'/></a></li>
                     <li><a href='https://github.com/justo-marcella'><img src='img/github.png' alt='link para github'/></a></li>
